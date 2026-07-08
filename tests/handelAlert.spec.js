7897e37dd921b92a4cc69ec3a4ef3fc141274782
Rakesh <rakeshavula29@gmail.com>
2026-07-08 13:22:34 +0530

    updeted to aletr

--- tests/handelAlert.spec.js
+++ tests/handelAlert.spec.js
@@ -5,7 +5,7 @@ test('handel alert',async({page})=>{
     page.on('dialog',async(dialog)=>{
         console.log('alter messs'+dialog.message())
         await dialog.accept()
-    })
+    }) 
     await page.click("//button[@id='alertBtn']")
 })
 test('handel alters',async({page})=>{
@@ -15,4 +15,12 @@ test('handel alters',async({page})=>{
         await dialog.dismiss()
     })
     await page.click("//button[@id='confirmBtn']")
+})
+test('handel alterss',async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    page.on('dailog,',async(dialog)=>{
+
+        await dialog.accept('Rakesh')
+    })
+    await page.click("//button[@id='promptBtn']")
 })
