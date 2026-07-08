167349ea43d5b02203e304548a8e2a2007d955ec
Rakesh <rakeshavula29@gmail.com>
2026-07-08 12:58:30 +0530

    Added handle alert test

---
+++ tests/handelAlert.spec.js
@@ -0,0 +1,18 @@
+import{test,expect}from"@playwright/test"
+
+test('handel alert',async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    page.on('dialog',async(dialog)=>{
+        console.log('alter messs'+dialog.message())
+        await dialog.accept()
+    })
+    await page.click("//button[@id='alertBtn']")
+})
+test('handel alters',async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    page.on('dailog,',async(dialog)=>{
+        console.log('altermsf'+dialog.message())
+        await dialog.dismiss()
+    })
+    await page.click("//button[@id='confirmBtn']")
+})
